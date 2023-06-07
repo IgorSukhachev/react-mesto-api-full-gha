062b8f60d7285be74efb157e44c55aaefdafff41
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-07 18:30:14 +0300

    )

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,10 @@ const signUpRouter = require('./signUp');
 
 const NotFound = require('../errors/NotFound');
 
-router.use('/api/', signInRouter);
-router.use('/api/', signUpRouter);
-router.use('/api/users', userRouter);
-router.use('/api/cards', cardRouter);
+router.use('/', signInRouter);
+router.use('/', signUpRouter);
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
 router.use((req, res, next) => {
   next(new NotFound('NOT FOUND'));
 });
