14819e069f386c5561fc3b155458dc6c9d198aba
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-08 18:04:53 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const cors = require('cors');
+const cors = require('./middlewares/cors');
 const router = require('./routes');
 const error = require('./middlewares/error');
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors())
+app.use(cors)
 
 app.use(router);
 app.use(errors());
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,28 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://mesto-suhachov.nomoredomains.rocks',
+  'http://mesto-suhachov.nomoredomains.rocks',
+  'localhost:3000'
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  return next();
+}
