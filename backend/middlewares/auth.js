847f940017c7924881dcd329f339268aa7a39713
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-09 19:08:16 +0300

    headers

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
 const authorization = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.headers.authorization;
   if (!token) {
     next(new Unauthorized('Пользователь не авторизован'));
   }
