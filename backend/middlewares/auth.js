7067b2dec6e94a00b52a68df069a5e4e732bf96b
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-09 19:09:32 +0300

    xd

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
 const authorization = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authrorization;
   if (!token) {
     next(new Unauthorized('Пользователь не авторизован'));
   }
