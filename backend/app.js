4082d89c30f97c86b5b1e7a10d3c33f036b2040a
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-10 21:01:59 +0300

    logger

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ const helmet = require('helmet');
 const cors = require('cors');
 const router = require('./routes');
 const error = require('./middlewares/error');
-const {requestLogger, errorLogger} = require("./middlewares/logger");
 
 const { PORT = 3000 } = process.env;
 
@@ -27,8 +26,6 @@ app.use(
   }),
 );
 
-app.use(requestLogger);
-
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
@@ -36,7 +33,6 @@ app.get('/crash-test', () => {
 });
 
 app.use(router);
-app.use(errorLogger);
 app.use(errors());
 app.use(error);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const signInRouter = require('./signIn');
 const signUpRouter = require('./signUp');
 
 const NotFound = require('../errors/NotFound');
+
+router.use(requestLogger);
 
 router.use('/', signInRouter);
 router.use('/', signUpRouter);
@@ -15,5 +19,7 @@ router.use('/cards', cardRouter);
 router.use((req, res, next) => {
   next(new NotFound('NOT FOUND'));
 });
+
+router.use(errorLogger);
 
 module.exports = router;
