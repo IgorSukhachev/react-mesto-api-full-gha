f56d2ec546ebb46430428227aae514c19ecc78e7
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-10 18:02:07 +0300

    try env

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -12,6 +12,8 @@ const InternalServerError = require('../errors/InternalServerError');
 
 const { CastError, ValidationError, DocumentNotFoundError } = mongoose.Error;
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const getUser = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
@@ -92,7 +94,7 @@ const login = (req, res, next) => {
         // eslint-disable-next-line consistent-return
         .then((matched) => {
           if (matched) {
-            const token = jwt.sign({ _id: user._id }, 'SECRET_KEY', { expiresIn: '7d' });
+            const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
             return res.send({ token });
           }
           return next(new Unauthorized('Invalid email or password'));
