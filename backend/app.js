8e2008762417d71f6befd94dce570ffdd1a667b1
Igor Suhachov <42006386+IgorSuhachov@users.noreply.github.com>
2023-06-10 18:10:18 +0300

    try to fix

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-// require('dotenv').config();
 const express = require('express');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,3 @@
-// require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -12,8 +11,6 @@ const Conflict = require('../errors/Conflict');
 const InternalServerError = require('../errors/InternalServerError');
 
 const { CastError, ValidationError, DocumentNotFoundError } = mongoose.Error;
-
-// const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUser = (req, res, next) => {
   User.find({})
